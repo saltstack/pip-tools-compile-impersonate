6f47e50b146a369dd0f18b37710c45f16b6ee624
Pedro Algarvio <pedro@algarvio.me>
2019-03-24 14:48:54 +0000

    First stab at a pre-commit hook

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='pip-tools-compile',
+      version='1.0',
+      install_requires=['six', 'pip-tools==3.5.0', 'pip==19.0.3'],
+      scripts=['pip-tools-compile'])
