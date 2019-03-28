e97cf8c118b025ead552dc5f7a684b8e38c66cfb
Pedro Algarvio <pedro@algarvio.me>
2019-03-28 14:02:40 +0000

    Switch to using mock instead of explicitly patching

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup
 
-setup(name='pip-tools-compile',
-      version='1.0',
-      install_requires=['six', 'pip-tools==3.5.0', 'pip==19.0.3'],
-      scripts=['pip-tools-compile'])
+setup(
+    name='pip-tools-compile',
+    version='1.0',
+    install_requires=[
+        'six',
+        'pip-tools==3.5.0',
+        'pip==19.0.3',
+        'mock>=2.0.0; python_version < \'3\''
+    ],
+    scripts=['pip-tools-compile'])
