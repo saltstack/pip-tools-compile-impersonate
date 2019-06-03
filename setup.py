bfc7ed4635557e1b160213b7e2d3ee0a548bebbd
Pedro Algarvio <pedro@algarvio.me>
2019-06-03 09:28:47 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version='1.0',
     install_requires=[
         'six',
-        'pip-tools==3.5.0',
-        'pip==19.0.3',
+        'pip-tools==3.6.1',
+        'pip==19.1',
         'mock>=2.0.0; python_version < \'3\''
     ],
     scripts=['pip-tools-compile'])
