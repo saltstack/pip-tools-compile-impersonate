97ac74a8a6f10c7ca1d44344a2e371ee6182eab5
Megan Wilhite <megan.wilhite@gmail.com>
2021-04-27 10:28:22 -0400

    install rust

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
         'six',
         'pip-tools==3.6.1',
         'pip==19.1',
+        'setuptools-rust',
         'mock>=2.0.0; python_version < \'3\''
     ],
     scripts=['pip-tools-compile'])
